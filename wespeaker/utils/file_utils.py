4604602f4630d4714a884ec1913e44f092da1fb0
Hongji Wang <jijijiang77@gmail.com>
2022-05-25 16:55:13 +0800

    [wespeaker] refine asnorm codes

--- wespeaker/utils/file_utils.py
+++ wespeaker/utils/file_utils.py
@@ -2,21 +2,36 @@
 # coding=utf-8
 # Author: Hongji Wang
 
+
 def read_scp(scp_file):
+    """ scp_file: mostly 2 columns
+    """
     key_value_list = []
-    with open(scp_file, "r") as fin:
-        line = fin.readline()
-        while line:
+    with open(scp_file, "r", encoding='utf8') as fin:
+        for line in fin:
             tokens = line.strip().split()
             key = tokens[0]
             value = " ".join(tokens[1:])
             key_value_list.append((key, value))
-            line = fin.readline()
     return key_value_list
 
+
 def read_lists(list_file):
+    """ list_file: only 1 column
+    """
     lists = []
     with open(list_file, 'r', encoding='utf8') as fin:
         for line in fin:
             lists.append(line.strip())
     return lists
+
+
+def read_table(table_file):
+    """ table_file: any columns
+    """
+    table_list = []
+    with open(table_file, 'r', encoding='utf8') as fin:
+        for line in fin:
+            tokens = line.strip().split()
+            table_list.append(tokens)
+    return table_list
