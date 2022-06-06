983b9749bbb73d0deaa1324dc46da6c612c0f369
Binbin Zhang <binbzha@qq.com>
2022-06-06 23:59:05 +0800

    [wespeaker] fix license in uio

--- wespeaker/dataset/processor.py
+++ wespeaker/dataset/processor.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2022 Horizon Robtics. (authors: Binbin Zhang)
+# Copyright (c) 2021 Mobvoi Inc. (authors: Binbin Zhang)
+#               2022 Chengdong Liang (liangchengdong@mail.nwpu.edu.cn)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
