ee02460bd02805085d5c54be7f169f47b000bbc0
Shuai Wang <wsstriving@gmail.com>
2022-06-13 23:47:35 +0800

    fix the quich-checks failure

--- wespeaker/bin/export_onnx.py
+++ wespeaker/bin/export_onnx.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, NVIDIA CORPORATION.  
+# Copyright (c) 2022, NVIDIA CORPORATION.
 #                     Shuai Wang (wsstriving@gmail.com)
 # All rights reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
