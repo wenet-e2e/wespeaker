6f154aa039a781df0ff22abd81627d1772f39876
Hongji Wang <jijijiang77@gmail.com>
2022-06-14 22:07:10 +0800

    [onnx] update onnx codes (#55)
    * [onnx] update onnx code to support both deprecated IO and UIO methods
    
    * [onnx] add onnxruntime in requirements.txt

--- wespeaker/bin/export_onnx.py
+++ wespeaker/bin/export_onnx.py
@@ -64,8 +64,12 @@ def main():
 
     model = Model(model, mean_vec)
     model.eval()
+
     feat_dim = configs['model_args'].get('feat_dim', 80)
-    num_frms = configs['dataset_args'].get('num_frms', 200)
+    if 'feature_args' in configs:  # deprecated IO
+        num_frms = configs['feature_args'].get('num_frms', 200)
+    else:  # UIO
+        num_frms = configs['dataset_args'].get('num_frms', 200)
 
     dummy_input = torch.ones(1, num_frms, feat_dim)
     torch.onnx.export(
