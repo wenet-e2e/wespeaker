616aabf5c524588051bbf7f617260738f5dbe932
Zhengyang Chen <chenzhengyang117@gmail.com>
2022-07-13 15:09:58 +0800

    [example] add large margin finetuning to cnsrc recipe and add the deep resnet definition (#66)
    * [example] add large margin finetuning to cnsrc recipe
    
    * fix lint
    
    * add more description

--- examples/cnceleb/v2/README.md
+++ examples/cnceleb/v2/README.md
@@ -2,12 +2,15 @@
 
 * Setup: fbank80, num_frms200, ArcMargin, aug_prob0.6, speed_perturb (no spec_aug)
 * test_trials: CNC-Eval-Core.lst
-* 🔥 UPDATE: We update this recipe according to the setups in the winning system of CNSRC 2022, and get obvious performance improvement compared with the old recipe. Check the [commit](https://github.com/wenet-e2e/wespeaker/pull/63/commits/b08804987b3bbb26f4963cedf634058474c743dd) for details.
+* 🔥 UPDATE: We update this recipe according to the setups in the winning system of CNSRC 2022, and get obvious performance improvement compared with the old recipe. Check the [commit1](https://github.com/wenet-e2e/wespeaker/pull/63/commits/b08804987b3bbb26f4963cedf634058474c743dd),[commit2](https://github.com/wenet-e2e/wespeaker/pull/66/commits/6f6af29197f0aa0a5d1b1993b7feb2f41b97891f) for details.
     * LR scheduler warmup from 0
     * Remove one embedding layer
+    * add large margin fine-tuning strategy (LM)
 
 | Model                      | Params   | EER (%)   | minDCF (p=0.01) |
 | :------------------------- | :------: | :-------: | :-------------: |
 | OLD: ResNet34-TSTP-emb256-ep150 | 6.70M    | 8.426     | 0.487           |
-| NEW: ResNet34-TSTP-emb256-ep165 | 6.63M    | 7.181     | 0.408           |
-| NEW: ResNet34-TSTP-emb256-ep165+AS-Norm | 6.63M    | 6.899     | 0.367          |
+| NEW: ResNet34-TSTP-emb256-ep150 | 6.63M    | 7.033     | 0.412           |
+| NEW: ResNet34-TSTP-emb256-ep150+AS-Norm | 6.63M    | 6.747     | 0.371          |
+| NEW: ResNet34-TSTP-emb256-ep150+LM | 6.63M    | 6.517     | 0.395           |
+| NEW: ResNet34-TSTP-emb256-ep150+LM+AS-Norm | 6.63M    | 6.466     | 0.357          |
--- examples/cnceleb/v2/run.sh
+++ examples/cnceleb/v2/run.sh
@@ -2,6 +2,7 @@
 
 # Copyright 2022 Hongji Wang (jijijiang77@gmail.com)
 #           2022 Chengdong Liang (liangchengdong@mail.nwpu.edu.cn)
+#           2022 Zhengyang Chen (chenzhengyang117@gmail.com)
 
 . ./path.sh || exit 1;
 
@@ -11,15 +12,23 @@ stop_stage=-1
 data=data
 data_type="shard"  # shard/raw
 
-gpus="[0,1]"
 config=conf/resnet.yaml
-exp_dir=exp/ResNet34-TSTP-emb256-fbank80-num_frms200-aug0.6-spTrue-saFalse-ArcMargin-SGD-epoch165
-num_avg=1
+exp_dir=exp/ResNet34-TSTP-emb256-fbank80-num_frms200-aug0.6-spTrue-saFalse-ArcMargin-SGD-epoch150
+gpus="[0,1]"
+num_avg=10
 checkpoint=
 
-trials="CNC-Eval-Concat.lst CNC-Eval-Avg.lst"
 score_norm_method="asnorm"  # asnorm/snorm
 top_n=300
+trials="CNC-Eval-Concat.lst CNC-Eval-Avg.lst"
+
+# setup for large margin fine-tuning
+do_lm=0
+lm_config=conf/resnet_lm.yaml
+lm_stage=3
+lm_stop_stage=7
+lm_exp_dir=exp/ResNet34-TSTP-emb256-fbank80-num_frms200-aug0.6-spTrue-saFalse-ArcMargin-SGD-epoch150_LM
+lm_num_avg=1
 
 . tools/parse_options.sh || exit 1
 
@@ -49,61 +58,49 @@ if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
   python tools/make_lmdb.py ${data}/rirs/wav.scp ${data}/rirs/lmdb
 fi
 
-if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-  echo "Start training ..."
-  num_gpus=$(echo $gpus | awk -F ',' '{print NF}')
-  torchrun --standalone --nnodes=1 --nproc_per_node=$num_gpus \
-    wespeaker/bin/train.py --config $config \
-      --exp_dir ${exp_dir} \
-      --gpus $gpus \
-      --num_avg ${num_avg} \
-      --data_type "${data_type}" \
-      --train_data ${data}/cnceleb_train/${data_type}.list \
-      --train_label ${data}/cnceleb_train/utt2spk \
-      --reverb_data ${data}/rirs/lmdb \
-      --noise_data ${data}/musan/lmdb \
-      ${checkpoint:+--checkpoint $checkpoint}
+# stage 3 to stage 7, model training and inference
+bash local/train_eval.sh --stage ${stage} \
+                --stop_stage ${stop_stage} \
+                --data ${data} \
+                --data_type ${data_type} \
+                --config ${config} \
+                --exp_dir ${exp_dir} \
+                --gpus ${gpus} \
+                --num_avg ${num_avg} \
+                --checkpoint ${checkpoint} \
+                --score_norm_method ${score_norm_method} \
+                --top_n ${top_n} \
+                --trials ${trials}
+
+
+# ================== Large margin fine-tuning ==================
+# for reference: https://arxiv.org/abs/2206.11699
+# It shoule be noted that the large margin fine-tuning
+# is optional. It often be used in speaker verification
+# challenge to further improve performance. This training
+# proces will take longer segment as input and will take
+# up more gpu memory.
+
+
+if [ ${do_lm} -ne 1 ];
+    exit 1
 fi
 
-if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
-  echo "Do model average ..."
-  avg_model=$exp_dir/models/avg_model.pt
-  python wespeaker/bin/average_model.py \
-    --dst_model $avg_model \
-    --src_path $exp_dir/models \
-    --num ${num_avg}
+# using the pre-trained average model to initialize the LM training
+lm_model_dir=${lm_exp_dir}/models
+mkdir -p ${lm_model_dir}
+cp $exp_dir/models/avg_model.pt ${lm_model_dir}/model_0.pt
 
-  echo "Extract embeddings ..."
-  local/extract_cnc.sh \
-    --exp_dir $exp_dir --model_path $avg_model \
-    --nj 16 --gpus $gpus --data_type $data_type --data ${data}
-fi
-
-if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
-  echo "Score ..."
-  local/score.sh \
-    --stage 1 --stop-stage 2 \
-    --data ${data} \
-    --exp_dir $exp_dir \
-    --trials "$trials"
-fi
-
-if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
-  echo "Score norm ..."
-  local/score_norm.sh \
-    --stage 1 --stop-stage 3 \
-    --score_norm_method $score_norm_method \
-    --cohort_set cnceleb_train \
-    --top_n $top_n \
-    --data ${data} \
-    --exp_dir $exp_dir \
-    --trials "$trials"
-fi
-
-if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
-  echo "Export the best model ..."
-  python wespeaker/bin/export_jit.py \
-    --config $exp_dir/config.yaml \
-    --checkpoint $exp_dir/models/avg_model.pt \
-    --output_file $exp_dir/models/final.zip
-fi
+# stage 3 to stage 7, model training and inference
+bash local/train_eval.sh --stage ${lm_stage} \
+                --stop_stage ${lm_stop_stage} \
+                --data ${data} \
+                --data_type ${data_type} \
+                --config ${lm_config} \
+                --exp_dir ${lm_exp_dir} \
+                --gpus ${gpus} \
+                --num_avg ${lm_num_avg} \
+                --checkpoint ${lm_model_dir}/model_0.pt \
+                --score_norm_method ${score_norm_method} \
+                --top_n ${top_n} \
+                --trials ${trials}
