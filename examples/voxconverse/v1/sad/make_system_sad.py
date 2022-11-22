a9694fac859b1fce91dcca13095954889c2602b7
czy97 <chenzhengyang117@gmail.com>
2022-11-22 17:06:35 +0800

    [voxconverse] delele repeated files

--- examples/voxconverse/v1/sad/make_system_sad.py
+++ examples/voxconverse/v1/sad/make_system_sad.py
@@ -14,6 +14,12 @@
 
 
 import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
 import sys
 import functools
 import concurrent.futures
@@ -90,11 +96,6 @@ def main():
 
 
 if __name__ == '__main__':
-    os.environ["OMP_NUM_THREADS"] = "1"
-    os.environ["OPENBLAS_NUM_THREADS"] = "1"
-    os.environ["MKL_NUM_THREADS"] = "1"
-    os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
-    os.environ["NUMEXPR_NUM_THREADS"] = "1"
     torch.set_num_threads(1)
 
     main()
