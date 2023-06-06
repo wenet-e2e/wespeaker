abacbeffe7945d474a718b2d3a0dccf3c462320a
xx205 <xx205@outlook.com>
2023-06-06 21:46:27 +0800

    Add a recipe for VoxSRC-23's diarization track

--- examples/voxconverse/v2/diar/make_fbank.py
+++ examples/voxconverse/v2/diar/make_fbank.py
@@ -14,6 +14,12 @@
 # limitations under the License.
 
 import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["NUMBA_NUM_THREADS"] = "1"
 import argparse
 from collections import OrderedDict
 import kaldiio
