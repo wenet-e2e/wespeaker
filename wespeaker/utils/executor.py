895057ffd4937c9af516df1bceafc28be38de8bd
czy97 <chenzhengyang117@gmail.com>
2023-07-11 09:25:57 +0800

    merge from master

--- wespeaker/utils/executor.py
+++ wespeaker/utils/executor.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from contextlib import nullcontext
 import tableprint as tp
 
 import torch
@@ -21,7 +20,7 @@ import torchnet as tnt
 
 
 def run_epoch(dataloader,
-              loader_size,
+              epoch_iter,
               model,
               criterion,
               optimizer,
@@ -38,54 +37,50 @@ def run_epoch(dataloader,
     loss_meter = tnt.meter.AverageValueMeter()
     acc_meter = tnt.meter.ClassErrorMeter(accuracy=True)
 
-    # https://github.com/wenet-e2e/wenet/blob/main/wenet/utils/executor.py#L40
-    if isinstance(model, torch.nn.parallel.DistributedDataParallel):
-        model_context = model.join
-    else:
-        model_context = nullcontext
+    for i, batch in enumerate(dataloader):
+        utts = batch['key']
+        targets = batch['label']
+        features = batch['feat']
 
-    with torch.set_grad_enabled(True), model_context():
-        for i, batch in enumerate(dataloader):
-            utts = batch['key']
-            targets = batch['label']
-            features = batch['feat']
+        cur_iter = (epoch - 1) * epoch_iter + i
+        scheduler.step(cur_iter)
+        margin_scheduler.step(cur_iter)
 
-            cur_iter = (epoch - 1) * loader_size + i
-            scheduler.step(cur_iter)
-            margin_scheduler.step(cur_iter)
+        features = features.float().to(device)  # (B,T,F)
+        targets = targets.long().to(device)
 
-            features = features.float().to(device)  # (B,T,F)
-            targets = targets.long().to(device)
+        with torch.cuda.amp.autocast(enabled=enable_amp):
+            outputs = model(features)  # (embed_a,embed_b) in most cases
+            embeds = outputs[-1] if isinstance(outputs, tuple) else outputs
+            outputs = model.module.projection(embeds, targets)
+            if isinstance(outputs, tuple):
+                outputs, loss = outputs
+            else:
+                loss = criterion(outputs, targets)
 
-            with torch.cuda.amp.autocast(enabled=enable_amp):
-                outputs = model(features)  # (embed_a,embed_b) in most cases
-                embeds = outputs[-1] if isinstance(outputs, tuple) else outputs
-                outputs = model.module.projection(embeds, targets)
-                if isinstance(outputs, tuple):
-                    outputs, loss = outputs
-                else:
-                    loss = criterion(outputs, targets)
+        # loss, acc
+        loss_meter.add(loss.item())
+        acc_meter.add(outputs.cpu().detach().numpy(),
+                      targets.cpu().numpy())
 
-            # loss, acc
-            loss_meter.add(loss.item())
-            acc_meter.add(outputs.cpu().detach().numpy(),
-                          targets.cpu().numpy())
+        # updata the model
+        optimizer.zero_grad()
+        # scaler does nothing here if enable_amp=False
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
 
-            # updata the model
-            optimizer.zero_grad()
-            # scaler does nothing here if enable_amp=False
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
+        # log
+        if (i + 1) % log_batch_interval == 0:
+            logger.info(
+                tp.row((epoch, i + 1, scheduler.get_lr(),
+                        margin_scheduler.get_margin()) +
+                       (loss_meter.value()[0], acc_meter.value()[0]),
+                       width=10,
+                       style='grid'))
 
-            # log
-            if (i + 1) % log_batch_interval == 0:
-                logger.info(
-                    tp.row((epoch, i + 1, scheduler.get_lr(),
-                            margin_scheduler.get_margin()) +
-                           (loss_meter.value()[0], acc_meter.value()[0]),
-                           width=10,
-                           style='grid'))
+        if (i + 1) == epoch_iter:
+            break
 
     logger.info(
         tp.row((epoch, i + 1, scheduler.get_lr(),
