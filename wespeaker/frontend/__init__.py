173e24b775894c1c7a972047c477e7391ca69aaa
Hongji Wang <jijijiang77@gmail.com>
2024-07-06 17:31:33 +0800

    [feature] add a frontend module in wespeaker and support wavlm

---
+++ wespeaker/frontend/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# coding:utf-8
+# Author: Hongji Wang
+# Email: hongjiwang@tencent.com
+# Created on: 20240625
+
+from .s3prl import S3prlFrontend
+
+frontend_class_dict = {'s3prl':S3prlFrontend}
