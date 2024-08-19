f894bb265fcecd1fa09af1891819ce3707e013c1
xx205 <xx205@outlook.com>
2024-08-19 16:20:24 +0000

    link to local and path.sh; update requirements.txt and extract_emb.py

--- examples/voxconverse/v3/path.sh
+++ examples/voxconverse/v3/path.sh
@@ -1,5 +1,1 @@
-export PATH=$PWD:$PATH
-
-# NOTE(kan-bayashi): Use UTF-8 in Python to avoid UnicodeDecodeError when LC_ALL=C
-export PYTHONIOENCODING=UTF-8
-export PYTHONPATH=../../../:$PYTHONPATH
+../v2/path.sh
