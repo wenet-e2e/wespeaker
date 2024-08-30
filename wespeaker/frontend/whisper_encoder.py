72998ef597e2c7843f642eab8ad80993f1080ca6
Aurora1818 <zhaoyy22@mails.tsinghua.edu.cn>
2024-08-30 11:48:22 +0800

    Support Whisper-PMFA

--- wespeaker/frontend/whisper_encoder.py
+++ wespeaker/frontend/whisper_encoder.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Hongji Wang (jijijiang77@gmail.com)
+# Copyright (c) 2024 Yiyang Zhao (zhaoyy22@mails.tsinghua.edu.cn)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
