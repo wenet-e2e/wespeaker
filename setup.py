c047286bfafb09ee263600f8f52aac02c089b041
Hongji Wang <jijijiang77@gmail.com>
2024-10-12 11:06:45 +0800

    [cli] add cli requirements of umap+hdbscan (#372)
    * [cli] add cli requirements of umap+hdbscan
    
    * [cli] fix cpplint=1.6.1 in .github/workflows/lint.yml

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 requirements = [
     "tqdm",
     "kaldiio",
+    "hdbscan==0.8.37",
+    "umap-learn==0.5.6",
     "torch>=1.12.0",
     "torchaudio>=0.12.0",
     "silero-vad",
