7bdac3f3b652523f6c8b4365a7b017020dd31fc5
cdliang11 <1404056823@qq.com>
2026-03-16 18:36:25 +0800

    [deps] fix deps

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ from setuptools import setup, find_packages
 requirements = [
     "tqdm",
     "kaldiio",
-    "hdbscan==0.8.37",
+    "hdbscan>=0.8.37",
     "umap-learn==0.5.6",
     "torch>=1.12.0",
     "torchaudio>=0.12.0",
     "silero-vad",
+    "s3prl",
+    "openai-whisper",
+    "peft",
+    "accelerate",
 ]
 
 setup(
